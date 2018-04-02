df467705b36832dd703c31823f5ebfcdcae511b6
failedcoder12 <divyanshverma12@gmail.com>
2018-04-02 18:41:16 +0530

    fetch

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -28,7 +28,25 @@ MongoClient.connect('mongodb://localhost:27017', (err, client)=> {
 	// 	console.log(JSON.stringify(result.ops,undefined,2));
 	// 	console.log(result.ops[0]._id.getTimestamp());
 	// });
+	// db.collection('Todos').find({completed: false}).toArray().then((docs)=>{
+	// 	console.log('Todos');
+	// 	console.log(JSON.stringify(docs,undefined,2));
+	// },(err) => {
+	// 	console.log('Unable to fetch todos',err);
+	// });
 
-	client.close();
+	// db.collection('Todos').find({_id: new ObjectID('5ac229b6bce7030e2b9d33ce')}).toArray().then((docs)=>{
+	// 	console.log('Todos');
+	// 	console.log(JSON.stringify(docs,undefined,2));
+	// },(err) => {
+	// 	console.log('Unable to fetch todos',err);
+	// });
+
+	db.collection('Todos').find().count().then((count)=>{
+		console.log('Todos count: ',count);
+	},(err) => {
+		console.log('Unable to fetch todos',err);
+	});
+	// client.close();
 });
 
