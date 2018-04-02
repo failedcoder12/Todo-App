89abf824da2be09af6a90cb4db9aabf5952f0fd5
failedcoder12 <divyanshverma12@gmail.com>
2018-04-02 19:00:05 +0530

    delete

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -17,6 +17,8 @@ MongoClient.connect('mongodb://localhost:27017', (err, client)=> {
 
 	console.log('Connected to MongoDB server');
 
+	//DB insert----------------------------------------------------------------------
+
 	// db.collection('Todos').insertOne({
 	// 	text: 'Something to do',
 	// 	completed: false
@@ -28,6 +30,9 @@ MongoClient.connect('mongodb://localhost:27017', (err, client)=> {
 	// 	console.log(JSON.stringify(result.ops,undefined,2));
 	// 	console.log(result.ops[0]._id.getTimestamp());
 	// });
+
+	//DB find --------------------------------------------------------------------------
+
 	// db.collection('Todos').find({completed: false}).toArray().then((docs)=>{
 	// 	console.log('Todos');
 	// 	console.log(JSON.stringify(docs,undefined,2));
@@ -42,11 +47,31 @@ MongoClient.connect('mongodb://localhost:27017', (err, client)=> {
 	// 	console.log('Unable to fetch todos',err);
 	// });
 
-	db.collection('Todos').find().count().then((count)=>{
-		console.log('Todos count: ',count);
-	},(err) => {
-		console.log('Unable to fetch todos',err);
-	});
+	//DB count ---------------------------------------------------------------------------
+
+	// db.collection('Todos').find().count().then((count)=>{
+	// 	console.log('Todos count: ',count);
+	// },(err) => {
+	// 	console.log('Unable to fetch todos',err);
+	// });
 	// client.close();
+
+	// DB delete-------------------------------------------------------------------------
+
+	//delete many
+
+	// db.collection('Todos').deleteMany({text: 'Eat Lunch'}).then((result)=>{
+	// 	console.log(result);
+	// });
+
+	//delete one
+	// db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result)=>{
+	// 	console.log(result);
+	// });
+
+	//findONe and delete
+	db.collection('Todos').findOneAndDelete({completed: true}).then((result)=>{
+		console.log(result);
+	});
 });
 
